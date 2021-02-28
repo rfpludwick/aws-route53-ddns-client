40a901466e648335ae47f0200132801cf9605925
Robert F.P. Ludwick <rfpludwick@gmail.com>
2021-02-28 05:19:41 +0000

    Devcontainer added

---
+++ .devcontainer/provision.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+wget https://golang.org/dl/go1.16.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.16.linux-amd64.tar.gz
+rm go1.16.linux-amd64.tar.gz
+echo PATH="${PATH}":/usr/local/go/bin > /etc/profile.d/02-golang.sh
+# shellcheck disable=SC1091
+source /etc/profile
+pushd src/go
+go get -v golang.org/x/tools/gopls
+go get -v github.com/uudashr/gopkgs/v2/cmd/gopkgs
+go get -v github.com/ramya-rao-a/go-outline
+go get -v github.com/go-delve/delve/cmd/dlv
+go get -v golang.org/x/lint/golint
+go get -v golang.org/x/tools/gopls
+popd
+apt update
+apt install -y php
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+
+# Local provisioner?
+if [ -x .devcontainer/provision-local.sh ]; then
+	.devcontainer/provision-local.sh
+fi
--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
 # Todo
 
-- [ ] Error checking and appropriate defaults for config INI files
-- [ ] Write PHPUnit tests
+- Error checking and appropriate defaults for config INI files
+- Write PHPUnit tests
